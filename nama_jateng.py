16d2ba457f60f04c16cc9d853ab437a32a970193
Feriman0540 <Feriman11@gmail.com>
2024-03-07 19:17:03 +0700

    kelompok

---
+++ nama_jateng.py
@@ -0,0 +1,28 @@
+def nama_jateng(nama_list):
+    for nama in nama_list:
+        print(nama)
+
+
+nama_kelompok = ["Aditya Hadi Wijaya",
+"Muhammad Alfani Akbar Bahtiar",
+"Arina Fatunnisa",
+"Hendy Kurniawan",
+"Fransiskus Astha Adji Nugraha",
+"Siti Salwa Sajida",
+"Alfian Nova Handhika",
+"Maelatul Fitroh",
+"Heksa Falaq Amarulla",
+"Dani Arya Wijaya",
+"Ilham Habib Setiawan",
+"Adham Faraitodi",
+"L. D. Ikwal Hidayat",
+"Putri Cathliniya Diyanti Faliha",
+"Feriman",
+"Achmad Sandi Aji Permadi",
+"Wisnu Fathul Munir",
+"Rohmatulloh Fadhilah",
+"Hasyim Ashari Abdul Mukti",
+"Farhan Burhanudin Saifullah"]
+
+
+nama_jateng(nama_kelompok)
